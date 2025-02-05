27ee52d99a01a1a724b4858e7c5969340e528ffb
harshkanaujia28 <154357957+harshkanaujia28@users.noreply.github.com>
2025-02-05 17:48:52 +0530

    img

--- src/Component/HeroSection.jsx
+++ src/Component/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HeroSection.css";
-import { assets } from "../assets/assets"; 
+import { assets } from "../../public/assets"; 
 
 const HeroSection = () => {
   return (
